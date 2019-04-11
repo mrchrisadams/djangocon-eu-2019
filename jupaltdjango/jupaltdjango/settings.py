d7ce8d69a323f4733e28bfd10642e0ce7c0ab953
Chris Adams <chris@productscience.co.uk>
2019-04-11 09:51:41 +0200

    relax setting for demo

--- jupaltdjango/jupaltdjango/settings.py
+++ jupaltdjango/jupaltdjango/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# naughty
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
