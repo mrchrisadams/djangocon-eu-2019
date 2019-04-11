1732f27e579950961407001acae0a87dbab47dc1
Chris Adams <chris@productscience.co.uk>
2019-04-11 09:47:58 +0200

    DOH! Forgot the web server!

--- jupaltdjango/jupaltdjango/settings.py
+++ jupaltdjango/jupaltdjango/settings.py
@@ -40,10 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'charts',
     'django_extensions',
+    "gunicorn",
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -123,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
